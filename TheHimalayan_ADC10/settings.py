a3056573c1b5f55c1b94a5a02587be74b8709d37
NischalBatas <58929522+NischalBatas@users.noreply.github.com>
2020-01-08 14:25:27 +0545

    Crud Operation Complelte

--- TheHimalayan_ADC10/settings.py
+++ TheHimalayan_ADC10/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MEDIA_ROOT = BASE_DIR+'/media'
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
